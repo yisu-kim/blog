db2af7a7974ca92c0c2968660244515ed3174927
yisu-kim <yisu.kim.dev@outlook.com>
2021-02-18 13:05:12 +0900

    [Style] Code Highlight 테마 추가 및 스타일 변경

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,6 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.618,
   googleFonts: [
     {
       name: "Noto Serif KR",
@@ -12,12 +10,11 @@ const typography = new Typography({
       name: "Noto Sans KR",
       styles: ["400", "700"],
     },
+    {
+      name: "Fira Code",
+      styles: ["400"],
+    },
   ],
-  headerFontFamily: ["Noto Sans KR", "sans-serif"],
-  bodyFontFamily: ["Noto Serif KR", "serif"],
-  headerWeight: 700,
-  bodyWeight: 400,
-  boldWeight: 700,
 })
 
 export default typography
