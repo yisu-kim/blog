fff9029393eb934eefaeaf76a42c9e47b14a298b
yisu-kim <yisu.kim.dev@outlook.com>
2021-03-07 23:41:45 +0900

    [Feat] Layout 컴포넌트에 Header와 Footer 사용

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,42 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
+import { Container, CssBaseline, ThemeProvider } from "@material-ui/core"
+import muiTheme from "../mui-theme"
+import Header from "./header"
+import Footer from "./footer"
 
 config.autoAddCss = false
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  const pageRegex = new RegExp("/page/\\d+")
-  const isPagePath = pageRegex.test(location.pathname)
-  let header
-
-  if (isRootPath || isPagePath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+export default function Layout({ children }) {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          author {
+            name
+            summary
+          }
+          source
+        }
+      }
+    }
+  `)
+  const {
+    site: {
+      siteMetadata: {
+        title,
+        author: { name },
+        source,
+      },
+    },
+  } = data
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer className="global-footer">
-        © {new Date().getFullYear()}. Yisu Kim all rights reserved. Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={muiTheme}>
+        <Header title={title} source={source} />
+        <Container maxWidth="sm">{children}</Container>
+        <Footer name={name} />
+      </ThemeProvider>
+    </>
   )
 }
-
-export default Layout
