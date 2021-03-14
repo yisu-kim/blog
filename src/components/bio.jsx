0e73510d55b8bf624ad0d4779bc07314749b5924
yisu-kim <yisu.kim.dev@outlook.com>
2021-03-14 10:54:20 +0900

    [Feat] Bio 컴포넌트 MUI 적용

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -7,10 +7,27 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import {
+  Container,
+  IconButton,
+  makeStyles,
+  Typography,
+} from "@material-ui/core"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-const Bio = () => {
+const useStyles = makeStyles(theme => ({
+  bio: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: theme.spacing(3),
+  },
+  githubButton: {
+    marginLeft: theme.spacing(1),
+  },
+}))
+
+export default function Bio() {
+  const classes = useStyles()
   const data = useStaticQuery(graphql`
     query BioQuery {
       site {
@@ -34,26 +51,20 @@ const Bio = () => {
   } = data
 
   return (
-    <div className="bio">
-      <p>
-        {name && (
-          <span>
-            Written by <strong>{name}</strong> {summary || null}
-          </span>
-        )}
-        {github && (
-          <a
-            href={`https://github.com/${github}`}
-            aria-label="Github"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        )}
-      </p>
-    </div>
+    <Container className={classes.bio}>
+      <Typography variant="body2" component="span">
+        Written by <strong>{name}</strong> {summary || null}
+      </Typography>
+      {github && (
+        <IconButton
+          className={classes.githubButton}
+          href={`https://github.com/${github}`}
+          aria-label="GitHub"
+          color="inherit"
+        >
+          <GitHubIcon />
+        </IconButton>
+      )}
+    </Container>
   )
 }
-
-export default Bio
